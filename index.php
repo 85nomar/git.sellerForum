6601c53beefce8e93ee93e21a5b1d5ddda8317f4
Nobel Martinez <nobel.martinez@ricardo.ch>
2013-06-20 12:27:28 +0200

    changed visual in homepage

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $Layout->start(); ?>
             <div class="visual-text">
               <h1>GET API ACCESS</h1>
               <p>Make a step forward and get your API access. Increase your sales revenue and reduce the administration cost<p>
-              <a class="btn btn-primary btn-large" href="request.html">Request Form! »</a></p>
+              <a class="btn btn-primary btn-large" href="request.html">Make a Request »</a></p>
             </div>
             <div class="visual-img">
               <img src="img/visual-home.png" />
@@ -30,35 +30,49 @@ $Layout->start(); ?>
     <div class="row-fluid">
       <div class="span4">
         <h3>ricardo.ch announces new API</h3>
-        <p>
-            Today we are announcing the launch of our new API developer site and the new Ricardo API. Great news for Sellers, you will be able to connect your ERP to Ricardo platform with  a
-        </p>
-        <ul>
-          <li>Simpler implementation.</li>
-          <li>Clear documentation and code samples.</li>
-          <li>Supporting tools.</li>
-          <li> Support Forums.</li>
-        </ul>
-        <p>Most importantly, this is just a starting point. We will continue to add new features based on what we
-            need and what we learn. Feel free to write your thoughts in Forum section.</p>
-        <p><a class="btn-anchor" href="news.html"><i class="icon-angle-right"></i>View details</a>
-        </p>
+        <div class="row-fluid">
+            <div class="posted-date date-box span2">
+                <span class="month">Jun</span>
+                <span class="day">19</span>
+                <span class="year">2013</span>
+            </div>
+            <div class="span9">
+                <p>We are announcing the launch of our new API developer site and the new Ricardo API. Great news for sellers.</p>
+                <p>Most importantly, this is just the beginning. We will continuously add new. The features are based on the learning.</p>
+                <p>Feel free to write your thoughts in the forum section.</p>
+                <!-- <p><a class="btn-anchor" href="news.html"><i class="icon-angle-right"></i>View details</a></p> -->
+            </div>
+        </div>
       </div>
       <div class="span4">
         <h3>API roadmap</h3>
-        <p>
-            It’s going to be a busy summer and therefore we already release this version of the new API so the planning can start on your side. The API development will be an ongoing process and you’ll find here the latest changes and updates which will be implemented during this summer. From beginning of 1st July you’ll be able to connect your shop to our test environment to test it and make sure all works fine.        </p>
-        <p> Feel free to write your thoughts in Forum section</p>
-            <p><a class="btn-anchor" href="news.html"><i class="icon-angle-right"></i>View details</a></p>
+          <div class="row-fluid">
+              <div class="posted-date date-box span2">
+                  <span class="month">Jun</span>
+                  <span class="day">19</span>
+                  <span class="year">2013</span>
+              </div>
+              <div class="span9">
+                  <p>The development of the API will be an ongoing process and you will find here the latest changes and updates which will be implemented during this summer.</p>
+                  <p>In this first step, you will be able to get a better overview of the new documentation and from beginning of 1st July you will be able to connect to the API</p>
+                  <!-- <p><a class="btn-anchor" href="news.html"><i class="icon-angle-right"></i>View details</a></p> -->
+              </div>
+          </div>
       </div>
       <div class="span4">
         <h3>How to get access to the API</h3>
-          <p>We have changed the registration procedure to make it more simple and fast for you to get started.
-              Please fill out the request form which you find <a href="request.html">here</a></p>
-
-        <p>
-           <a class="btn-anchor" href="news.html"><i class="icon-angle-right"></i>View details</a>
-        </p>
+          <div class="row-fluid">
+              <div class="posted-date date-box span2">
+                  <span class="month">Jun</span>
+                  <span class="day">19</span>
+                  <span class="year">2013</span>
+              </div>
+              <div class="span9">
+                  <p>We have changed the registration procedure to make it easier and more comfortable to get access to our API.</p>
+                  <p>You will find the new “request form” directly on our website. Please provide us all the necessary information, so we can process your request as quickly as possible. It is important to submit us the request even though you have already get access to the old API.</p>
+                  <!-- <p><a class="btn-anchor" href="news.html"><i class="icon-angle-right"></i>View details</a></p> -->
+              </div>
+          </div>
       </div>
     </div>
   </div>
